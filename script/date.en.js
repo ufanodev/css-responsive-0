4eaaa2005782e931787ae02f64998d3104314ab3
Ufano Dev <ufano.dev@gmail.com>
2022-12-11 10:31:41 +0100

    start css-responsive-0

---
+++ script/date.en.js
@@ -0,0 +1,16 @@
+﻿var months = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+
+var WeekDays = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+
+
+
+var f = new Date();  //javascript
+
+var strDate = WeekDays[f.getDay()] + " - " + months[f.getMonth()] + " " + f.getDate() + ", " + f.getFullYear();
+
+$(function () {
+    /* con el método jQuery adecuado, asignar  el string strDate que contiene la fecha obtenida con la función Date() de javascript a un elemento HTML que has de incluir en el footer para tal efecto */
+    /* For this task, only  "FULL YEAR" */
+    $('footer span').last().text(f.getFullYear());
+});
+
---
+++ script/jQuery_mas_media.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+
+
+    function update() {
+        $("h3").text("Viewport Size....." + $(window).width() + " x " + $(window).height() );
+    }
+
+    update();
+
+    /* Window event. Resize browser window */
+    $(window).resize(function () { update(); });
+
+
+});
+
+
+
+
